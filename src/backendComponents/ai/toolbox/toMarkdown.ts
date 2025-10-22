a41ae49bcad1a857a3177bf14267e832a2d2186f
jmbish04 <jmbish04@gmail.com>
2025-10-21 18:07:12 -0700

    Add toMarkdown function to handle PDF and JPEG

---
+++ src/backendComponents/ai/toolbox/toMarkdown.ts
@@ -0,0 +1,28 @@
+import { Env } from "./env";
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    // https://pub-979cb28270cc461d94bc8a169d8f389d.r2.dev/somatosensory.pdf
+    const pdf = await env.R2.get("somatosensory.pdf");
+
+    // https://pub-979cb28270cc461d94bc8a169d8f389d.r2.dev/cat.jpeg
+    const cat = await env.R2.get("cat.jpeg");
+
+    return Response.json(
+      await env.AI.toMarkdown([
+        {
+          name: "somatosensory.pdf",
+          blob: new Blob([await pdf.arrayBuffer()], {
+            type: "application/octet-stream",
+          }),
+        },
+        {
+          name: "cat.jpeg",
+          blob: new Blob([await cat.arrayBuffer()], {
+            type: "application/octet-stream",
+          }),
+        },
+      ]),
+    );
+  },
+};
